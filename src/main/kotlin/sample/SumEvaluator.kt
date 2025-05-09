555c932db0cbd61acf6305b01070564656049dd8
Vecarlos <vercarlos@brightsector.com>
2025-05-09 10:55:13 -0300

    kotlin implementation

---
+++ src/main/kotlin/sample/SumEvaluator.kt
@@ -0,0 +1,8 @@
+package sample
+
+// SumEvaluator.kt
+class SumEvaluator {
+  fun evaluate(a: Int, b: Int): Int{
+    return a + b
+  }
+}
---
+++ src/test/kotlin/sample/CountLettersEvaluatorTest.kt
@@ -0,0 +1,25 @@
+package sample
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import org.junit.Assert.assertEquals
+
+@RunWith(JUnit4::class)
+class CountLettersEvaluatorTest {
+
+  private val evaluator = CountLettersEvaluator()
+
+  @Test
+  fun `when string is "hello", then returns  5`() {
+    val result = evaluator.evaluate("hello")
+    assertEquals(5, result)
+  }
+
+  @Test
+  fun `when string is empty, then returns  0`() {
+    val result = evaluator.evaluate("")
+    assertEquals(0, result)
+  }
+
+}
