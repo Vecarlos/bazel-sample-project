223c4f3d5568c2322d31902b836519c0e81ca689
Vecarlos <vercarlos@brightsector.com>
2025-05-09 16:22:37 -0300

    fix pytype

--- tools/pytype/run-check.sh
+++ tools/pytype/run-check.sh
@@ -9,7 +9,7 @@ export PATH="${python_bin_dir}:${ninja_bin_dir}:$PATH"
 
 python_paths=$(find "${BUILD_WORKING_DIRECTORY}/src/main/python" -type d | paste -sd:)
 
-python_files_to_analyze=$(find "${BUILD_WORKING_DIRECTORY}/src/main/python" "${BUILD_WORKING_DIRECTORY}/src/test/python" -name '*.py' ! -name '__init__.py') 
+python_files_to_analyze=$(find "${BUILD_WORKING_DIRECTORY}/src/main/python"  -name '*.py' ! -name '__init__.py') 
 for file in ${python_files_to_analyze}
 do
   echo -e "\n${file}"
