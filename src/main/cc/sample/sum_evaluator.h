39eeec89beffa3f7c23d34eaf737a7f97f26c6b5
Vecarlos <vercarlos@brightsector.com>
2025-05-14 09:46:07 -0300

    cc implementation

---
+++ src/main/cc/sample/sum_evaluator.h
@@ -0,0 +1,15 @@
+#ifndef SRC_MAIN_CPP_SAMPLE_SUM_EVALUATOR_H_
+#define SRC_MAIN_CPP_SAMPLE_SUM_EVALUATOR_H_
+
+namespace sample
+{
+
+  class SumEvaluator
+  {
+  public:
+    static int evaluate(int a, int b);
+  };
+
+}
+
+#endif
