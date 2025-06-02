e2eedd409e8a44acff14fc0bf88653951f25ff62
Vecarlos <vercarlos@brightsector.com>
2025-06-02 11:53:06 -0300

    code changes + test

--- src/main/kotlin/client/CountLettersClientApp.kt
+++ src/main/kotlin/client/CountLettersClientApp.kt
@@ -1,32 +1,26 @@
 package client
 // reference https://github.com/world-federation-of-advertisers/cross-media-measurement/blob/d2d8b14b8d3c08b26938f3721849e10e8056a9a1/src/main/kotlin/org/wfanet/measurement/duchy/deploy/common/job/mill/shareshuffle/HonestMajorityShareShuffleMillJob.kt#L42
 import kotlinx.coroutines.runBlocking
-import picocli.CommandLine
-import kotlin.system.exitProcess
-import client.Flags.CountLettersClientFlags
 
-@CommandLine.Command(
-    name = "CountLettersClientApp",
-    description = ["gRPC client for CountLettersService"],
-    mixinStandardHelpOptions = true,
-    showDefaultValues = true
-)
-class CountLettersClientApp : Runnable {
-    @CommandLine.Mixin
-    lateinit var flags: CountLettersClientFlags
-        private set
-
-    override fun run() {
-        val client = CountLettersClient(flags.host, flags.port)
-        runBlocking {
-        client.run(flags.textToCount)
-        }
-    }
-}
-
-fun runClientApp(vararg args: String): Int {
-    return CommandLine(CountLettersClientApp()).execute(*args)
-}
 fun main(args: Array<String>) {
-    System.exit(runClientApp(*args))
+    val host: String
+    val port: Int
+    val textToCount: String
+
+    if (args.size == 3){
+        host = args[0]
+        port = args[1].toIntOrNull() ?: run {
+            println("ERROR: Port must be a valid number")
+            return
+        }
+        textToCount = args[2]
+    } else {
+        host = "localhost"
+        port = 50051
+        textToCount = args[0]
+    }
+    val client = CountLettersClient(host, port)
+    runBlocking {
+        client.run(textToCount)
+    }
 }
