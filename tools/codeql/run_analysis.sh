956b740b745d4466789b0e25699cf077cf34292d
Vecarlos <vercarlos@brightsector.com>
2025-06-27 10:30:47 -0300

    codeql + buildbuddy

--- tools/codeql/run_analysis.sh
+++ tools/codeql/run_analysis.sh
@@ -4,13 +4,15 @@ set -e
 LANGUAGE="$LANGUAGE"
 CODEQL_EXEC="./external/_main~_repo_rules~codeql_cli/codeql"
 
+echo "--- Creating CodeQL database for ${LANGUAGE} by tracing the Bazel build ---"
 "${CODEQL_EXEC}" database create \
   --language="${LANGUAGE}" \
-  --command="bazel build --spawn_strategy=local //..." \
+  --command="bazel build \
+    --config=ci \
+    --fetch=false \
+    --spawn_strategy=local \
+    //..." \
   codeql-db
-
-# El paso "echo '--- Building code...' " y "bazel build..." que tenías antes
-# ya no son necesarios, porque ahora CodeQL ejecuta el build por nosotros.
 
 echo "--- Analyzing database ---"
 "${CODEQL_EXEC}" database analyze --format=sarif-latest --output=results.sarif codeql-db
