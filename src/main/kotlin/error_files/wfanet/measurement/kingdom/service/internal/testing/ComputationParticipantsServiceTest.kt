442487bb0d4934eec49fef4ed185ccfe7ffb2ee8
Vecarlos <vercarlos@brightsector.com>
2025-07-15 17:56:21 -0300

    reduce code

--- src/main/kotlin/error_files/wfanet/measurement/kingdom/service/internal/testing/ComputationParticipantsServiceTest.kt
+++ src/main/kotlin/error_files/wfanet/measurement/kingdom/service/internal/testing/ComputationParticipantsServiceTest.kt
@@ -13,86 +13,31 @@
 // limitations under the License.
 
 package org.wfanet.measurement.kingdom.service.internal.testing
-
-import com.google.common.truth.Truth.assertThat
-import com.google.common.truth.extensions.proto.ProtoTruth.assertThat
-import com.google.protobuf.ByteString
-import io.grpc.Status
-import io.grpc.StatusRuntimeException
-import java.time.Clock
-import java.time.temporal.ChronoUnit
 import kotlin.random.Random
-import kotlin.test.assertFailsWith
-import kotlin.test.assertNotNull
-import kotlinx.coroutines.flow.toList
-import kotlinx.coroutines.runBlocking
+
+
+import java.time.Clock
 import org.junit.Before
-import org.junit.BeforeClass
-import org.junit.Rule
-import org.junit.Test
+
+
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
-import org.wfanet.measurement.api.Version
-import org.wfanet.measurement.common.grpc.errorInfo
+
 import org.wfanet.measurement.common.identity.IdGenerator
 import org.wfanet.measurement.common.identity.RandomIdGenerator
-import org.wfanet.measurement.common.toInstant
-import org.wfanet.measurement.internal.kingdom.AccountsGrpcKt.AccountsCoroutineImplBase
-import org.wfanet.measurement.internal.kingdom.Certificate
-import org.wfanet.measurement.internal.kingdom.CertificatesGrpcKt.CertificatesCoroutineImplBase
-import org.wfanet.measurement.internal.kingdom.ComputationParticipant
-import org.wfanet.measurement.internal.kingdom.ComputationParticipantDetails
-import org.wfanet.measurement.internal.kingdom.ComputationParticipantsGrpcKt.ComputationParticipantsCoroutineImplBase
-import org.wfanet.measurement.internal.kingdom.DataProvider
-import org.wfanet.measurement.internal.kingdom.DataProvidersGrpcKt.DataProvidersCoroutineImplBase
-import org.wfanet.measurement.internal.kingdom.DuchyProtocolConfig
-import org.wfanet.measurement.internal.kingdom.ErrorCode
-import org.wfanet.measurement.internal.kingdom.FulfillRequisitionRequestKt.computedRequisitionParams
-import org.wfanet.measurement.internal.kingdom.HonestMajorityShareShuffleParams
-import org.wfanet.measurement.internal.kingdom.Measurement
-import org.wfanet.measurement.internal.kingdom.MeasurementConsumer
+
 import org.wfanet.measurement.internal.kingdom.MeasurementConsumersGrpcKt.MeasurementConsumersCoroutineImplBase
-import org.wfanet.measurement.internal.kingdom.MeasurementFailure
+import org.wfanet.measurement.internal.kingdom.DataProvidersGrpcKt.DataProvidersCoroutineImplBase
 import org.wfanet.measurement.internal.kingdom.MeasurementsGrpcKt.MeasurementsCoroutineImplBase
-import org.wfanet.measurement.internal.kingdom.ProtocolConfig
-import org.wfanet.measurement.internal.kingdom.Requisition
+import org.wfanet.measurement.internal.kingdom.CertificatesGrpcKt.CertificatesCoroutineImplBase
 import org.wfanet.measurement.internal.kingdom.RequisitionsGrpcKt.RequisitionsCoroutineImplBase
-import org.wfanet.measurement.internal.kingdom.StreamRequisitionsRequestKt.filter
-import org.wfanet.measurement.internal.kingdom.cancelMeasurementRequest
-import org.wfanet.measurement.internal.kingdom.computationParticipant
-import org.wfanet.measurement.internal.kingdom.confirmComputationParticipantRequest
-import org.wfanet.measurement.internal.kingdom.copy
-import org.wfanet.measurement.internal.kingdom.failComputationParticipantRequest
-import org.wfanet.measurement.internal.kingdom.fulfillRequisitionRequest
-import org.wfanet.measurement.internal.kingdom.getComputationParticipantRequest
-import org.wfanet.measurement.internal.kingdom.getMeasurementByComputationIdRequest
-import org.wfanet.measurement.internal.kingdom.honestMajorityShareShuffleParams
-import org.wfanet.measurement.internal.kingdom.liquidLegionsV2Params
-import org.wfanet.measurement.internal.kingdom.revokeCertificateRequest
-import org.wfanet.measurement.internal.kingdom.setMeasurementResultRequest
-import org.wfanet.measurement.internal.kingdom.setParticipantRequisitionParamsRequest
-import org.wfanet.measurement.internal.kingdom.streamRequisitionsRequest
-import org.wfanet.measurement.kingdom.deploy.common.HmssProtocolConfig
-import org.wfanet.measurement.kingdom.deploy.common.Llv2ProtocolConfig
-import org.wfanet.measurement.kingdom.deploy.common.testing.DuchyIdSetter
-import org.wfanet.measurement.kingdom.service.internal.testing.Population.Companion.DUCHIES
+import org.wfanet.measurement.internal.kingdom.AccountsGrpcKt.AccountsCoroutineImplBase
+import org.wfanet.measurement.internal.kingdom.ComputationParticipantsGrpcKt.ComputationParticipantsCoroutineImplBase
+
 
 private const val RANDOM_SEED = 1
-private const val PROVIDED_MEASUREMENT_ID = "measurement"
-
-private val EL_GAMAL_PUBLIC_KEY = ByteString.copyFromUtf8("This is an ElGamal Public Key.")
-private val EL_GAMAL_PUBLIC_KEY_SIGNATURE =
-  ByteString.copyFromUtf8("This is an ElGamal Public Key signature.")
-private val TINK_PUBLIC_KEY = ByteString.copyFromUtf8("This is an Tink Public Key.")
-private val TINK_PUBLIC_KEY_SIGNATURE =
-  ByteString.copyFromUtf8("This is an Tink Public Key signature.")
-private const val TINK_PUBLIC_KEY_SIGNATURE_ALGORITHM_OID = "2.9999"
-
 @RunWith(JUnit4::class)
 abstract class ComputationParticipantsServiceTest<T : ComputationParticipantsCoroutineImplBase> {
-
-  @get:Rule val duchyIdSetter = DuchyIdSetter(DUCHIES)
-
   protected data class Services<T>(
     val computationParticipantsService: T,
     val measurementConsumersService: MeasurementConsumersCoroutineImplBase,
@@ -102,45 +47,15 @@ abstract class ComputationParticipantsServiceTest<T : ComputationParticipantsCor
     val requisitionsService: RequisitionsCoroutineImplBase,
     val accountsService: AccountsCoroutineImplBase,
   )
-
   private val clock: Clock = Clock.systemUTC()
   protected val idGenerator = RandomIdGenerator(clock, Random(RANDOM_SEED))
   private val population = Population(clock, idGenerator)
-
-  protected lateinit var computationParticipantsService: T
-    private set
-
-  private lateinit var duchyCertificates: Map<String, Certificate>
-
-  protected lateinit var measurementsService: MeasurementsCoroutineImplBase
-    private set
-
-  protected lateinit var measurementConsumersService: MeasurementConsumersCoroutineImplBase
-    private set
-
-  protected lateinit var dataProvidersService: DataProvidersCoroutineImplBase
-    private set
-
-  protected lateinit var certificatesService: CertificatesCoroutineImplBase
-    private set
-
-  protected lateinit var requisitionsService: RequisitionsCoroutineImplBase
-    private set
-
-  protected lateinit var accountsService: AccountsCoroutineImplBase
-    private set
 
   protected abstract fun newServices(idGenerator: IdGenerator): Services<T>
 
   @Before
   fun initService() {
     val services = newServices(idGenerator)
-    computationParticipantsService = services.computationParticipantsService
-    measurementConsumersService = services.measurementConsumersService
-    dataProvidersService = services.dataProvidersService
-    measurementsService = services.measurementsService
-    certificatesService = services.certificatesService
-    requisitionsService = services.requisitionsService
-    accountsService = services.accountsService
+
   }
 }
