2905d72a589aeba13d48bd13c3078fb9f801bcfa
Vecarlos <vercarlos@brightsector.com>
2025-07-15 16:36:16 -0300

    empty files

--- src/main/kotlin/replicate_error/fun_1.kt
+++ src/main/kotlin/replicate_error/fun_1.kt
@@ -1,3 +1,55 @@
 package replicate_error
-fun empty_func_1() {
+
+import org.junit.Before
+import org.junit.Test
+
+abstract class ApiKeysServiceTest<T : Int> {
+
+    // protected data class Services<T>(
+    //     val apiKeysService: T,
+    // )
+
+    @Before
+    fun initServices() {
+    }
+
+    @Test
+    fun `createApiKey with no description returns an api key`() {
+    }
+
+    @Test
+    fun `createApiKey with description returns an api key`() {
+    }
+
+    @Test
+    fun `createApiKey throws NOT_FOUND when measurement consumer doesn't exist`() {
+    }
+
+    @Test
+    fun `deleteApiKey returns the api key`() {
+    }
+
+    @Test
+    fun `deleteApiKey throws NOT_FOUND when measurement consumer doesn't exist`() {
+    }
+
+    @Test
+    fun `deleteApiKey throws NOT FOUND when api key doesn't exist`() {
+    }
+
+    @Test
+    fun `authenticateApiKey returns a measurement consumer`() {
+    }
+
+    @Test
+    fun `authenticateApiKey throws INVALID_ARGUMENT when authentication key hash is missing`() {
+    }
+
+    @Test
+    fun `authenticateApiKey throws NOT_FOUND when authentication key hash doesn't match`() {
+    }
+
+    @Test
+    fun `authenticateApiKey throws NOT_FOUND when api key has been deleted`() {
+    }
 }
--- src/test/kotlin/replicate_error/FunTest.kt
+++ src/test/kotlin/replicate_error/FunTest.kt
@@ -1,11 +1,51 @@
 package replicate_error
+
+import org.junit.Before
 import org.junit.Test
-import replicate_error.empty_func_1
+import replicate_error.ApiKeysServiceTest
 
-class MyFunctionsTest {
+abstract class ApiKeysServiceTest<T : Int> {
 
-    @Test
-    fun testDoNothing() {
-        empty_func_1()
-    }
+//   protected lateinit var services: Services<T>
+
+//   protected data class Services<T>(
+//     val apiKeysService: T
+//   )
+
+//   abstract fun newServices(idGenerator: IdGenerator): Services<T>
+
+  @Before
+  fun initServices() {
+  }
+
+  @Test
+  fun `createApiKey with no description returns an api key`() {
+  }
+
+  @Test
+  fun `createApiKey with description returns an api key`() {}
+
+  @Test
+  fun `createApiKey throws NOT_FOUND when measurement consumer doesn't exist`() {}
+
+  @Test
+  fun `deleteApiKey returns the api key`() {}
+
+  @Test
+  fun `deleteApiKey throws NOT_FOUND when measurement consumer doesn't exist`() {}
+
+  @Test
+  fun `deleteApiKey throws NOT FOUND when api key doesn't exist`() {}
+
+  @Test
+  fun `authenticateApiKey returns a measurement consumer`() {}
+
+  @Test
+  fun `authenticateApiKey throws INVALID_ARGUMENT when authentication key hash is missing`() {}
+
+  @Test
+  fun `authenticateApiKey throws NOT_FOUND when authentication key hash doesn't match`() {}
+
+  @Test
+  fun `authenticateApiKey throws NOT_FOUND when api key has been deleted`() {}
 }
