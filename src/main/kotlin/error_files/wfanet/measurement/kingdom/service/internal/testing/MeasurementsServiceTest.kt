796b4cd3c3c7f7ee32abde4f63571624c95ebe7c
Vecarlos <vercarlos@brightsector.com>
2025-07-15 17:27:12 -0300

    wfa comment all the file

--- src/main/kotlin/error_files/wfanet/measurement/kingdom/service/internal/testing/MeasurementsServiceTest.kt
+++ src/main/kotlin/error_files/wfanet/measurement/kingdom/service/internal/testing/MeasurementsServiceTest.kt
@@ -1,104 +1,104 @@
-// // Copyright 2021 The Cross-Media Measurement Authors
-// //
-// // Licensed under the Apache License, Version 2.0 (the "License");
-// // you may not use this file except in compliance with the License.
-// // You may obtain a copy of the License at
-// //
-// //      http://www.apache.org/licenses/LICENSE-2.0
-// //
-// // Unless required by applicable law or agreed to in writing, software
-// // distributed under the License is distributed on an "AS IS" BASIS,
-// // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// // See the License for the specific language governing permissions and
-// // limitations under the License.
+// Copyright 2021 The Cross-Media Measurement Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-// package org.wfanet.measurement.kingdom.service.internal.testing
+package org.wfanet.measurement.kingdom.service.internal.testing
 
-// import com.google.cloud.spanner.ErrorCode
-// import com.google.cloud.spanner.SpannerException
-// import com.google.common.truth.Truth.assertThat
-// import com.google.common.truth.extensions.proto.ProtoTruth.assertThat
-// import com.google.protobuf.ByteString
-// import io.grpc.Status
-// import io.grpc.StatusRuntimeException
-// import java.time.Clock
-// import java.time.Instant
-// import java.time.temporal.ChronoUnit
-// import kotlin.random.Random
-// import kotlin.test.assertFailsWith
-// import kotlinx.coroutines.flow.toList
-// import kotlinx.coroutines.runBlocking
-// import org.junit.Before
-// import org.junit.BeforeClass
-// import org.junit.Rule
-// import org.junit.Test
-// import org.junit.runner.RunWith
-// import org.junit.runners.JUnit4
-// import org.wfanet.measurement.api.Version
-// import org.wfanet.measurement.common.identity.IdGenerator
-// import org.wfanet.measurement.common.identity.RandomIdGenerator
-// import org.wfanet.measurement.common.toInstant
-// import org.wfanet.measurement.internal.kingdom.AccountsGrpcKt.AccountsCoroutineImplBase
-// import org.wfanet.measurement.internal.kingdom.CancelMeasurementRequest
-// import org.wfanet.measurement.internal.kingdom.Certificate
-// import org.wfanet.measurement.internal.kingdom.CertificatesGrpcKt
-// import org.wfanet.measurement.internal.kingdom.ComputationParticipant
-// import org.wfanet.measurement.internal.kingdom.ComputationParticipantDetails
-// import org.wfanet.measurement.internal.kingdom.DataProvider
-// import org.wfanet.measurement.internal.kingdom.DataProvidersGrpcKt.DataProvidersCoroutineImplBase
-// import org.wfanet.measurement.internal.kingdom.DeleteMeasurementRequest
-// import org.wfanet.measurement.internal.kingdom.DuchyProtocolConfig
-// import org.wfanet.measurement.internal.kingdom.Measurement
-// import org.wfanet.measurement.internal.kingdom.MeasurementConsumer
-// import org.wfanet.measurement.internal.kingdom.MeasurementConsumersGrpcKt.MeasurementConsumersCoroutineImplBase
-// import org.wfanet.measurement.internal.kingdom.MeasurementKt.resultInfo
-// import org.wfanet.measurement.internal.kingdom.MeasurementLogEntriesGrpcKt.MeasurementLogEntriesCoroutineImplBase
-// import org.wfanet.measurement.internal.kingdom.MeasurementLogEntryError
-// import org.wfanet.measurement.internal.kingdom.MeasurementsGrpcKt.MeasurementsCoroutineImplBase
-// import org.wfanet.measurement.internal.kingdom.ProtocolConfig
-// import org.wfanet.measurement.internal.kingdom.Requisition
-// import org.wfanet.measurement.internal.kingdom.Requisition.DuchyValue
-// import org.wfanet.measurement.internal.kingdom.RequisitionKt.parentMeasurement
-// import org.wfanet.measurement.internal.kingdom.RequisitionsGrpcKt.RequisitionsCoroutineImplBase
-// import org.wfanet.measurement.internal.kingdom.StreamMeasurementsRequestKt
-// import org.wfanet.measurement.internal.kingdom.StreamMeasurementsRequestKt.filter
-// import org.wfanet.measurement.internal.kingdom.StreamRequisitionsRequestKt
-// import org.wfanet.measurement.internal.kingdom.batchCancelMeasurementsRequest
-// import org.wfanet.measurement.internal.kingdom.batchCreateMeasurementsRequest
-// import org.wfanet.measurement.internal.kingdom.batchDeleteMeasurementsRequest
-// import org.wfanet.measurement.internal.kingdom.batchGetMeasurementsRequest
-// import org.wfanet.measurement.internal.kingdom.cancelMeasurementRequest
-// import org.wfanet.measurement.internal.kingdom.computationParticipant
-// import org.wfanet.measurement.internal.kingdom.copy
-// import org.wfanet.measurement.internal.kingdom.createDuchyMeasurementLogEntryRequest
-// import org.wfanet.measurement.internal.kingdom.createMeasurementRequest
-// import org.wfanet.measurement.internal.kingdom.deleteMeasurementRequest
-// import org.wfanet.measurement.internal.kingdom.duchyMeasurementLogEntryDetails
-// import org.wfanet.measurement.internal.kingdom.duchyMeasurementLogEntryStageAttempt
-// import org.wfanet.measurement.internal.kingdom.duchyProtocolConfig
-// import org.wfanet.measurement.internal.kingdom.getMeasurementByComputationIdRequest
-// import org.wfanet.measurement.internal.kingdom.getMeasurementRequest
-// import org.wfanet.measurement.internal.kingdom.measurement
-// import org.wfanet.measurement.internal.kingdom.measurementDetails
-// import org.wfanet.measurement.internal.kingdom.measurementKey
-// import org.wfanet.measurement.internal.kingdom.measurementLogEntry
-// import org.wfanet.measurement.internal.kingdom.measurementLogEntryDetails
-// import org.wfanet.measurement.internal.kingdom.measurementLogEntryError
-// import org.wfanet.measurement.internal.kingdom.protocolConfig
-// import org.wfanet.measurement.internal.kingdom.requisition
-// import org.wfanet.measurement.internal.kingdom.requisitionDetails
-// import org.wfanet.measurement.internal.kingdom.revokeCertificateRequest
-// import org.wfanet.measurement.internal.kingdom.setMeasurementResultRequest
-// import org.wfanet.measurement.internal.kingdom.stateTransitionMeasurementLogEntry
-// import org.wfanet.measurement.internal.kingdom.streamMeasurementsRequest
-// import org.wfanet.measurement.internal.kingdom.streamRequisitionsRequest
-// import org.wfanet.measurement.internal.kingdom.streamStateTransitionMeasurementLogEntriesRequest
-// import org.wfanet.measurement.kingdom.deploy.common.DuchyIds
-// import org.wfanet.measurement.kingdom.deploy.common.HmssProtocolConfig
-// import org.wfanet.measurement.kingdom.deploy.common.Llv2ProtocolConfig
-// import org.wfanet.measurement.kingdom.deploy.common.RoLlv2ProtocolConfig
-// import org.wfanet.measurement.kingdom.deploy.common.testing.DuchyIdSetter
-// import org.wfanet.measurement.kingdom.service.internal.testing.Population.Companion.DUCHIES
+import com.google.cloud.spanner.ErrorCode
+import com.google.cloud.spanner.SpannerException
+import com.google.common.truth.Truth.assertThat
+import com.google.common.truth.extensions.proto.ProtoTruth.assertThat
+import com.google.protobuf.ByteString
+import io.grpc.Status
+import io.grpc.StatusRuntimeException
+import java.time.Clock
+import java.time.Instant
+import java.time.temporal.ChronoUnit
+import kotlin.random.Random
+import kotlin.test.assertFailsWith
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.BeforeClass
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import org.wfanet.measurement.api.Version
+import org.wfanet.measurement.common.identity.IdGenerator
+import org.wfanet.measurement.common.identity.RandomIdGenerator
+import org.wfanet.measurement.common.toInstant
+import org.wfanet.measurement.internal.kingdom.AccountsGrpcKt.AccountsCoroutineImplBase
+import org.wfanet.measurement.internal.kingdom.CancelMeasurementRequest
+import org.wfanet.measurement.internal.kingdom.Certificate
+import org.wfanet.measurement.internal.kingdom.CertificatesGrpcKt
+import org.wfanet.measurement.internal.kingdom.ComputationParticipant
+import org.wfanet.measurement.internal.kingdom.ComputationParticipantDetails
+import org.wfanet.measurement.internal.kingdom.DataProvider
+import org.wfanet.measurement.internal.kingdom.DataProvidersGrpcKt.DataProvidersCoroutineImplBase
+import org.wfanet.measurement.internal.kingdom.DeleteMeasurementRequest
+import org.wfanet.measurement.internal.kingdom.DuchyProtocolConfig
+import org.wfanet.measurement.internal.kingdom.Measurement
+import org.wfanet.measurement.internal.kingdom.MeasurementConsumer
+import org.wfanet.measurement.internal.kingdom.MeasurementConsumersGrpcKt.MeasurementConsumersCoroutineImplBase
+import org.wfanet.measurement.internal.kingdom.MeasurementKt.resultInfo
+import org.wfanet.measurement.internal.kingdom.MeasurementLogEntriesGrpcKt.MeasurementLogEntriesCoroutineImplBase
+import org.wfanet.measurement.internal.kingdom.MeasurementLogEntryError
+import org.wfanet.measurement.internal.kingdom.MeasurementsGrpcKt.MeasurementsCoroutineImplBase
+import org.wfanet.measurement.internal.kingdom.ProtocolConfig
+import org.wfanet.measurement.internal.kingdom.Requisition
+import org.wfanet.measurement.internal.kingdom.Requisition.DuchyValue
+import org.wfanet.measurement.internal.kingdom.RequisitionKt.parentMeasurement
+import org.wfanet.measurement.internal.kingdom.RequisitionsGrpcKt.RequisitionsCoroutineImplBase
+import org.wfanet.measurement.internal.kingdom.StreamMeasurementsRequestKt
+import org.wfanet.measurement.internal.kingdom.StreamMeasurementsRequestKt.filter
+import org.wfanet.measurement.internal.kingdom.StreamRequisitionsRequestKt
+import org.wfanet.measurement.internal.kingdom.batchCancelMeasurementsRequest
+import org.wfanet.measurement.internal.kingdom.batchCreateMeasurementsRequest
+import org.wfanet.measurement.internal.kingdom.batchDeleteMeasurementsRequest
+import org.wfanet.measurement.internal.kingdom.batchGetMeasurementsRequest
+import org.wfanet.measurement.internal.kingdom.cancelMeasurementRequest
+import org.wfanet.measurement.internal.kingdom.computationParticipant
+import org.wfanet.measurement.internal.kingdom.copy
+import org.wfanet.measurement.internal.kingdom.createDuchyMeasurementLogEntryRequest
+import org.wfanet.measurement.internal.kingdom.createMeasurementRequest
+import org.wfanet.measurement.internal.kingdom.deleteMeasurementRequest
+import org.wfanet.measurement.internal.kingdom.duchyMeasurementLogEntryDetails
+import org.wfanet.measurement.internal.kingdom.duchyMeasurementLogEntryStageAttempt
+import org.wfanet.measurement.internal.kingdom.duchyProtocolConfig
+import org.wfanet.measurement.internal.kingdom.getMeasurementByComputationIdRequest
+import org.wfanet.measurement.internal.kingdom.getMeasurementRequest
+import org.wfanet.measurement.internal.kingdom.measurement
+import org.wfanet.measurement.internal.kingdom.measurementDetails
+import org.wfanet.measurement.internal.kingdom.measurementKey
+import org.wfanet.measurement.internal.kingdom.measurementLogEntry
+import org.wfanet.measurement.internal.kingdom.measurementLogEntryDetails
+import org.wfanet.measurement.internal.kingdom.measurementLogEntryError
+import org.wfanet.measurement.internal.kingdom.protocolConfig
+import org.wfanet.measurement.internal.kingdom.requisition
+import org.wfanet.measurement.internal.kingdom.requisitionDetails
+import org.wfanet.measurement.internal.kingdom.revokeCertificateRequest
+import org.wfanet.measurement.internal.kingdom.setMeasurementResultRequest
+import org.wfanet.measurement.internal.kingdom.stateTransitionMeasurementLogEntry
+import org.wfanet.measurement.internal.kingdom.streamMeasurementsRequest
+import org.wfanet.measurement.internal.kingdom.streamRequisitionsRequest
+import org.wfanet.measurement.internal.kingdom.streamStateTransitionMeasurementLogEntriesRequest
+import org.wfanet.measurement.kingdom.deploy.common.DuchyIds
+import org.wfanet.measurement.kingdom.deploy.common.HmssProtocolConfig
+import org.wfanet.measurement.kingdom.deploy.common.Llv2ProtocolConfig
+import org.wfanet.measurement.kingdom.deploy.common.RoLlv2ProtocolConfig
+import org.wfanet.measurement.kingdom.deploy.common.testing.DuchyIdSetter
+import org.wfanet.measurement.kingdom.service.internal.testing.Population.Companion.DUCHIES
 
 // private const val RANDOM_SEED = 1
 // private const val API_VERSION = "v2alpha"
