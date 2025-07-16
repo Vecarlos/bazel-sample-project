4666b9e2007d760664df4106c7e1d52468870c55
Vecarlos <vercarlos@brightsector.com>
2025-07-16 10:01:02 -0300

    reduce code

--- src/main/kotlin/error_files/wfanet/measurement/kingdom/service/internal/testing/ModelProvidersServiceTest.kt
+++ src/main/kotlin/error_files/wfanet/measurement/kingdom/service/internal/testing/ModelProvidersServiceTest.kt
@@ -63,4 +63,70 @@ abstract class ModelProvidersServiceTest {
   }
 
 
+  @Test
+  fun `listModelProviders returns all model providers ordered by external ID`(): Unit =
+    runBlocking {
+      val modelProvider1 = ModelProvider.getDefaultInstance()
+      val createdModelProvider1 = modelProvidersService.createModelProvider(modelProvider1)
+
+      idGenerator.internalId = InternalId(FIXED_GENERATED_INTERNAL_ID + 1L)
+      idGenerator.externalId = ExternalId(FIXED_GENERATED_EXTERNAL_ID + 1L)
+      val modelProvider2 = ModelProvider.getDefaultInstance()
+      val createdModelProvider2 = modelProvidersService.createModelProvider(modelProvider2)
+
+      val response: ListModelProvidersResponse =
+        modelProvidersService.listModelProviders(ListModelProvidersRequest.getDefaultInstance())
+
+      assertThat(response)
+        .isEqualTo(
+          listModelProvidersResponse {
+            modelProviders += createdModelProvider1
+            modelProviders += createdModelProvider2
+          }
+        )
+    }
+
+  @Test
+  fun `listModelProviders returns page size number of model providers after page token, and next page token when there are more results`():
+    Unit = runBlocking {
+    val modelProvider1 = ModelProvider.getDefaultInstance()
+    val createdModelProvider1 = modelProvidersService.createModelProvider(modelProvider1)
+
+    idGenerator.internalId = InternalId(FIXED_GENERATED_INTERNAL_ID + 1L)
+    idGenerator.externalId = ExternalId(FIXED_GENERATED_EXTERNAL_ID + 1L)
+    val modelProvider2 = ModelProvider.getDefaultInstance()
+    val createdModelProvider2 = modelProvidersService.createModelProvider(modelProvider2)
+
+    idGenerator.internalId = InternalId(FIXED_GENERATED_INTERNAL_ID + 2L)
+    idGenerator.externalId = ExternalId(FIXED_GENERATED_EXTERNAL_ID + 2L)
+    val modelProvider3 = ModelProvider.getDefaultInstance()
+    val createdModelProvider3 = modelProvidersService.createModelProvider(modelProvider3)
+
+    val response: ListModelProvidersResponse =
+      modelProvidersService.listModelProviders(
+        listModelProvidersRequest {
+          pageSize = 1
+          pageToken = listModelProvidersPageToken {
+            after =
+              ListModelProvidersPageTokenKt.after {
+                externalModelProviderId = createdModelProvider1.externalModelProviderId
+              }
+          }
+        }
+      )
+
+    assertThat(response)
+      .isEqualTo(
+        listModelProvidersResponse {
+          modelProviders += createdModelProvider2
+          nextPageToken = listModelProvidersPageToken {
+            after =
+              ListModelProvidersPageTokenKt.after {
+                externalModelProviderId = createdModelProvider3.externalModelProviderId
+              }
+          }
+        }
+      )
+  }
+
 }
