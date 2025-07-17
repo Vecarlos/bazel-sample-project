90de605d5c48ce75f95c893abc69b1b8b36ba922
Vecarlos <vercarlos@brightsector.com>
2025-07-17 10:57:35 -0300

    delete dataprovider content

--- src/main/kotlin/error_files/wfanet/measurement/kingdom/service/internal/testing/DataProvidersServiceTest.kt
+++ src/main/kotlin/error_files/wfanet/measurement/kingdom/service/internal/testing/DataProvidersServiceTest.kt
@@ -145,14 +145,37 @@ abstract class DataProvidersServiceTest<T : DataProvidersCoroutineImplBase> {
   }
 
   @Test
-  fun `test_7`() = runBlocking {
-    listOf(
-        dataProvidersService.createDataProvider(CREATE_DATA_PROVIDER_REQUEST),
-      )
-    dataProvidersService.getDataProvider(
+  fun `test_6`() = runBlocking {
+      assertFailsWith<StatusRuntimeException> {
+      dataProvidersService.getDataProvider(
         getDataProviderRequest { externalDataProviderId = 404L }
       )
+    }
+  }
+
+  // @Test
+  // fun `test_7`() = runBlocking {
+  //   listOf(
+  //       dataProvidersService.createDataProvider(CREATE_DATA_PROVIDER_REQUEST),
+  //     )
+  //   dataProvidersService.getDataProvider(
+  //       getDataProviderRequest { externalDataProviderId = 404L }
+  //     )
     
+  // }
+
+
+
+  @Test
+  fun `batchGetDataProviders returns DataProviders in request order`() {
+    val dataProviders = runBlocking {
+      listOf(
+        dataProvidersService.createDataProvider(CREATE_DATA_PROVIDER_REQUEST),
+      )
+    }
+    val response = runBlocking { dataProvidersService.getDataProvider(
+        getDataProviderRequest { externalDataProviderId = 404L }
+      ) }
   }
 
   @Test
