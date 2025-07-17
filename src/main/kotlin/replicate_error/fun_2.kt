2aa0e5d715698edfe22a9e25271564588f30a1cf
Vecarlos <vercarlos@brightsector.com>
2025-07-17 13:37:57 -0300

    replicat error

--- src/main/kotlin/replicate_error/fun_2.kt
+++ src/main/kotlin/replicate_error/fun_2.kt
@@ -1,3 +1,554 @@
 package replicate_error
-fun empty_func_2() {
-}
+
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import org.junit.Test
+import kotlinx.coroutines.runBlocking
+
+
+@RunWith(JUnit4::class)
+abstract class DataProvidersServiceTest{
+
+  @Test
+  fun `test_1`() {
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+
+
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+
+
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+
+
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+
+
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+
+
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+
+
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+
+
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+
+
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+  }}
