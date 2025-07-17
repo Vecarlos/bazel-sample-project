b8f95ba498bc61687b56dc6e4f2a94f6aad07784
Vecarlos <vercarlos@brightsector.com>
2025-07-17 18:17:46 -0300

    trying fix

--- src/test/kotlin/replicate_error/FunTest.kt
+++ src/test/kotlin/replicate_error/FunTest.kt
@@ -4,18 +4,17 @@ package replicate_error
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
+import org.junit.Assert.assertEquals
+import replicate_error.SumEvaluator
 
-/**
- * Esta es la clase que Bazel ejecutará.
- * 1. Es una clase normal (no abstracta).
- * 2. Tiene la anotación @RunWith para que JUnit la reconozca.
- */
+
 @RunWith(JUnit4::class)
 class FunTest {
+  private val evaluator = SumEvaluator()
 
-    @Test
-    fun `mi test vacio que funciona`() {
-        // Este test está vacío y ahora sí se ejecutará correctamente.
-        assert(true)
-    }
+  @Test
+  fun `when a = 1 and b = 2, then returns  3`() {
+    val result = evaluator.evaluate(1, 2)
+    assertEquals(3, result)
+  }
 }
