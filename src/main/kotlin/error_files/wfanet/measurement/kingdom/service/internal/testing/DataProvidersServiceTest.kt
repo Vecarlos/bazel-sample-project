f59bf9f52b731b833f77b498f649a7fa050e5eb8
Vecarlos <vercarlos@brightsector.com>
2025-07-17 12:38:15 -0300

    runblocking

--- src/main/kotlin/error_files/wfanet/measurement/kingdom/service/internal/testing/DataProvidersServiceTest.kt
+++ src/main/kotlin/error_files/wfanet/measurement/kingdom/service/internal/testing/DataProvidersServiceTest.kt
@@ -125,4 +125,15 @@ abstract class DataProvidersServiceTest{
     runBlocking {}
     runBlocking {}
     runBlocking {}
+
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
+    runBlocking {}
   }}
