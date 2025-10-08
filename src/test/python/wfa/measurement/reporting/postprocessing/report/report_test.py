8979d527c9e3430a3278935d0847a573ec39fd1c
Vecarlos <vercarlos@brightsector.com>
2025-10-08 12:14:15 -0300

    half report imports

--- src/test/python/wfa/measurement/reporting/postprocessing/report/report_test.py
+++ src/test/python/wfa/measurement/reporting/postprocessing/report/report_test.py
@@ -24,11 +24,11 @@ from noiseninja.noised_measurements import SetMeasurementsSpec
 # from report.report import Report
 # from report.report import build_measurement_set
 # from report.report import get_covers
-# from report.report import is_cover
-# from report.report import is_union_reach_consistent
-# from report.testing.validate_report import are_overlap_constraints_consistent
-# from report.testing.validate_report import get_sorted_list
-# from report.testing.validate_report import ordered_sets_to_sorted_list
+from report.report import is_cover
+from report.report import is_union_reach_consistent
+from report.testing.validate_report import are_overlap_constraints_consistent
+from report.testing.validate_report import get_sorted_list
+from report.testing.validate_report import ordered_sets_to_sorted_list
 
 # from src.main.proto.wfa.measurement.reporting.postprocessing.v2alpha import \
 #   report_post_processor_result_pb2
