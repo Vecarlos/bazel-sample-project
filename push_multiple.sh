d125379d796c62cfcba285841ecfb4605525771f
Vecarlos <vercarlos@brightsector.com>
2025-10-09 11:27:28 -0300

    report solver mc api test

--- push_multiple.sh
+++ push_multiple.sh
@@ -2,7 +2,7 @@
 
 # --- CONFIGURACIÓN ---
 # Cambia esta variable para el nombre base de tus commits
-COMMIT_BASE_NAME="all python test"
+COMMIT_BASE_NAME="report solver mc api test"
 
 # Nombre de la rama a la que quieres subir los cambios
 BRANCH_NAME="buildbuddy"
