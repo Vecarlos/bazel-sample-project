6dc628f09512ce958877e0c1a8ce29054f2fbca4
Vecarlos <vercarlos@brightsector.com>
2025-10-09 17:39:08 -0300

    empty Solver

--- push_multiple.sh
+++ push_multiple.sh
@@ -2,7 +2,7 @@
 
 # --- CONFIGURACIÓN ---
 # Cambia esta variable para el nombre base de tus commits
-COMMIT_BASE_NAME="solver test first, no assertLess, False and equal, report_mc_api_2_test no test"
+COMMIT_BASE_NAME="empty Solver"
 
 # Nombre de la rama a la que quieres subir los cambios
 BRANCH_NAME="buildbuddy"
