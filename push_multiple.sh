cab26beba6644666371fcbd95ad14d3f9b67a65a
Vecarlos <vercarlos@brightsector.com>
2025-10-14 15:38:21 -0300

    all collect all

--- push_multiple.sh
+++ push_multiple.sh
@@ -5,13 +5,13 @@ COMMIT_BASE_NAME="all collect all"
 BRANCH_NAME="buildbuddy"
 REMOTE_NAME="origin"
 
-DELAY_SECONDS=120
+DELAY_SECONDS=85
 
 echo ">>> 1. Creando commit real con 'git add .'"
 git add .
 git commit -m "$COMMIT_BASE_NAME" && git push $REMOTE_NAME $BRANCH_NAME
 
-for i in {2..8}
+for i in {2..6}
 do
   echo -e "\n--- Esperando $DELAY_SECONDS segundos para no interrumpir la CI anterior..."
   sleep $DELAY_SECONDS
