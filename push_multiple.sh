c4df5adee94285fd5033cde6af45892ac5ce44d7
Vecarlos <vercarlos@brightsector.com>
2025-10-16 09:56:32 -0300

    all collect all, cache and write coverage

--- push_multiple.sh
+++ push_multiple.sh
@@ -5,7 +5,7 @@ COMMIT_BASE_NAME="all collect all, cache and write coverage"
 BRANCH_NAME="buildbuddy"
 REMOTE_NAME="origin"
 
-DELAY_SECONDS=100
+DELAY_SECONDS=150
 
 echo ">>> 1. Creando commit real con 'git add .'"
 git add .
