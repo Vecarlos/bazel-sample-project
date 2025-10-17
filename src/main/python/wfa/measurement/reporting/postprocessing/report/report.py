9eea6ef4568aba9445e0c3b30614013598233b01
Vecarlos <vercarlos@brightsector.com>
2025-10-17 13:28:39 -0300

    all collect all, cache and write coverage, no logs

--- src/main/python/wfa/measurement/reporting/postprocessing/report/report.py
+++ src/main/python/wfa/measurement/reporting/postprocessing/report/report.py
@@ -14,32 +14,10 @@
 
 
 from noiseninja.solver import Solver
-# from src.main.proto.wfa.measurement.reporting.postprocessing.v2alpha import \
-#   report_post_processor_result_pb2
 
 
 
 class Report:
-  """Represents a full report with multiple MetricReports and set relationships.
-
-    This class aggregates multiple MetricReport objects, and the subset relation
-    between the the metrics.
-
-    Attributes:
-        _metric_reports: A dictionary mapping metric names (e.g., "MRC", "AMI")
-                         to their corresponding MetricReport objects.
-        _metric_subsets_by_parent: A dictionary defining subset relationships
-                                   between metrics. Each key is a parent metric,
-                                   and the value is a list of its child metrics.
-        _cumulative_inconsistency_allowed_edp_combinations: A set of EDP
-                                                            combinations for
-                                                            which inconsistencies
-                                                            in cumulative
-                                                            measurements are
-                                                            allowed. This is for
-                                                            TV measurements.
-        _population_size: The size of the population.
-    """
 
   def __init__(
       self,
@@ -48,17 +26,4 @@ class Report:
       cumulative_inconsistency_allowed_edp_combinations: set[str],
       population_size: float = 0.0,
   ):
-    """
-    Args:
-        metric_reports: a dictionary mapping metric types to a MetricReport
-        metric_subsets_by_parent: a dictionary containing subset
-            relationship between the metrics. .e.g. ami >= [custom, mrc]
-        cumulative_inconsistency_allowed_edps : a set containing edp keys that won't
-            be forced to have self cumulative reaches be increasing
-    """
-    self._metric_reports = metric_reports
-    self._metric_subsets_by_parent = metric_subsets_by_parent
-    self._cumulative_inconsistency_allowed_edp_combinations = (
-        cumulative_inconsistency_allowed_edp_combinations
-    )
     self._population_size = population_size
