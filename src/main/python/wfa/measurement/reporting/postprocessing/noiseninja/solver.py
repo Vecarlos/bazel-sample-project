2be331f68937db3742000718a902548163eb198e
Vecarlos <vercarlos@brightsector.com>
2025-10-20 10:46:42 -0300

    all collect all, cache and write coverage, no logs

--- src/main/python/wfa/measurement/reporting/postprocessing/noiseninja/solver.py
+++ src/main/python/wfa/measurement/reporting/postprocessing/noiseninja/solver.py
@@ -17,23 +17,18 @@ import numpy as np
 
 class Solver:
 
-  def __init__(self, set_measurement_spec):
+  def __init__(self):
     variable_index_by_set_id = {1: 0, 2: 1, 3: 2, 4: 3}
     self.num_variables = len(variable_index_by_set_id)
   
-    self._add_equals(set_measurement_spec, variable_index_by_set_id)
+    self._add_equals(variable_index_by_set_id)
 
-  def _add_equals(self, set_measurement_spec,variable_index_by_set_id):
+  def _add_equals(self,variable_index_by_set_id):
     variables =  np.zeros(self.num_variables)
     equal_set = [1, [4]]
-    # for equal_set in set_measurement_spec:
-      
-    #   variables = np.zeros(self.num_variables)
-    #   variables[variable_index_by_set_id[equal_set[0]]] = 1
- 
     variables.put([variable_index_by_set_id[i] for i in equal_set[1]], -1)
-    self._add_eq_term(variables, 0)
+    self._add_eq_term()
 
 
-  def _add_eq_term(self, variables: np.array, k: float):
+  def _add_eq_term(self):
     pass
