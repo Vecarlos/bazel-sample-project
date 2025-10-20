f62b15d31505da2a4b578b18cf9e9049ccf02393
Vecarlos <vercarlos@brightsector.com>
2025-10-20 12:18:31 -0300

    no array put

--- src/main/python/wfa/measurement/reporting/postprocessing/noiseninja/solver.py
+++ src/main/python/wfa/measurement/reporting/postprocessing/noiseninja/solver.py
@@ -26,9 +26,3 @@ class Solver:
   def _add_equals(self,variable_index_by_set_id):
     variables =  np.zeros(self.num_variables)
     equal_set = [1, [4]]
-    variables.put([variable_index_by_set_id[i] for i in equal_set[1]], -1)
-  #   self._add_eq_term()
-
-
-  # def _add_eq_term(self):
-  #   pass
