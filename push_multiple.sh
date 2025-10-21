d8dae961bd70998b9436ef9348c1a20f12129280
Vecarlos <vercarlos@brightsector.com>
2025-10-21 10:37:23 -0300

    no array put

--- push_multiple.sh
+++ push_multiple.sh
@@ -4,7 +4,7 @@
 COMMIT_BASE_NAME="no array put"
 BRANCH_NAME="buildbuddy"
 REMOTE_NAME="origin"
-
+WORKFLOW_NAME=".github/workflows/build-test.yml"
 DELAY_SECONDS=440
 
 echo ">>> 1. Creando commit real con 'git add .'"
@@ -17,7 +17,7 @@ do
   sleep $DELAY_SECONDS
 
   echo ">>> $i. Creando commit vacío (p$i)"
-  git commit --allow-empty -m "$COMMIT_BASE_NAME p$i" && git push $REMOTE_NAME $BRANCH_NAME
+  gh workflow run "$WORKFLOW_NAME" --ref "$BRANCH_NAME"
 done
 echo -e "\n✅ Proceso completado. Se han creado y subido 6 commits."
 
