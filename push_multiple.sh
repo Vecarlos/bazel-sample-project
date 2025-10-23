8ed5ee8a55ee4d32a3b67533ead5c5e502ee60e2
dev <vecarlos@brightsector.com>
2025-10-23 13:48:23 -0300

    Just code for error

--- push_multiple.sh
+++ push_multiple.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-COMMIT_BASE_NAME="no array put"
+COMMIT_BASE_NAME="Just code for error"
 BRANCH_NAME="buildbuddy"
 REMOTE_NAME="origin"
-DELAY_SECONDS=80
+DELAY_SECONDS=70
 
 echo ">>> 1. Add and commit"
 git add .
 git commit -m "$COMMIT_BASE_NAME" && git push $REMOTE_NAME $BRANCH_NAME
 
-for i in {2..6}
+for i in {2..7}
 do
   echo -e "\n--- Waiting $DELAY_SECONDS seconds"
   sleep $DELAY_SECONDS
