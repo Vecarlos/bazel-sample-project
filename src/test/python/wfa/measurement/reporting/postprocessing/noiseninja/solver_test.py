ce58c604c87eb09fcaeac5559771915206e775d3
dev <vecarlos@brightsector.com>
2025-10-23 13:27:50 -0300

    no array put

--- src/test/python/wfa/measurement/reporting/postprocessing/noiseninja/solver_test.py
+++ src/test/python/wfa/measurement/reporting/postprocessing/noiseninja/solver_test.py
@@ -16,13 +16,121 @@ import unittest
 
 from noiseninja.solver import Solver
 
+
 class SolverTest(unittest.TestCase):
+
+
   def test_solve_when_highs_solver_fails_to_converge(self):
+    solver = Solver()
 
-    spec = [[1, [4]]]
-    solver = Solver(spec)
 
-   
+if __name__ == "__main__":
+  unittest.main()# Copyright 2024 The Cross-Media Measurement Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+
+from noiseninja.solver import Solver
+
+
+class SolverTest(unittest.TestCase):
+
+
+  def test_solve_when_highs_solver_fails_to_converge(self):
+    solver = Solver()
+
+
+if __name__ == "__main__":
+  unittest.main()# Copyright 2024 The Cross-Media Measurement Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+
+from noiseninja.solver import Solver
+
+
+class SolverTest(unittest.TestCase):
+
+
+  def test_solve_when_highs_solver_fails_to_converge(self):
+    solver = Solver()
+
+
+if __name__ == "__main__":
+  unittest.main()# Copyright 2024 The Cross-Media Measurement Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+
+from noiseninja.solver import Solver
+
+
+class SolverTest(unittest.TestCase):
+
+
+  def test_solve_when_highs_solver_fails_to_converge(self):
+    solver = Solver()
+
+
+if __name__ == "__main__":
+  unittest.main()# Copyright 2024 The Cross-Media Measurement Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+
+from noiseninja.solver import Solver
+
+
+class SolverTest(unittest.TestCase):
+
+
+  def test_solve_when_highs_solver_fails_to_converge(self):
+    solver = Solver()
+
 
 if __name__ == "__main__":
   unittest.main()
