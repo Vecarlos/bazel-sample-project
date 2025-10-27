b3f847c87d22ae7748d15ce113222e9936bf791a
dev <vecarlos@brightsector.com>
2025-10-27 17:57:05 -0300

    only kotlin test

---
+++ multiple_push.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+POLLUTER_BRANCH="buildbuddy"
+VICTIM_BRANCH="buildbuddy_kt_cov"
+
+REMOTE_NAME="origin"
+CI_WAIT_TIME_SECONDS=1000
+
+TOTAL_RUNS=4
+
+for i in $(seq 1 $TOTAL_RUNS)
+do
+  echo -e "\n================ Cycle $i / $TOTAL_RUNS ==================="
+
+  echo "Running $POLLUTER_BRANCH"
+  git checkout $POLLUTER_BRANCH
+  git commit --allow-empty -m "$POLLUTER_BRANCH $i"
+  git push $REMOTE_NAME $POLLUTER_BRANCH
+
+  echo -e "\n~ Waiting $CI_WAIT_TIME_SECONDS seconds..."
+  sleep $CI_WAIT_TIME_SECONDS
+
+  echo "Running $VICTIM_BRANCH"
+  git checkout $VICTIM_BRANCH
+  git commit --allow-empty -m "$VICTIM_BRANCH $i"
+  git push $REMOTE_NAME $VICTIM_BRANCH
+
+  echo -e "\n~ Waiting $CI_WAIT_TIME_SECONDS seconds..."
+  sleep $CI_WAIT_TIME_SECONDS
+
+done
+
+echo -e "\n==========================================================="
+echo "Done"
