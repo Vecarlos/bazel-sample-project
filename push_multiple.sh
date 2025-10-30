18b5dc40f8ff99ed3fe8ffb2e266ab8e8f083960
dev <vecarlos@brightsector.com>
2025-10-30 11:01:37 -0300

    sleep

--- push_multiple.sh
+++ push_multiple.sh
@@ -5,11 +5,12 @@ SLEEP_SECONDS=30
 COMMIT_BASE_NAME="Fluctuation test commit"
 BRANCH_NAME="releases/fluctuation_test_$(date +%Y_%m_%d_%H_%M_%S)"
 REMOTE_NAME="origin"
+REFRESH_SECONDS=10 
 
 git checkout -b ${BRANCH_NAME}
 
-
 wait_for_workflow_completion() {
+  sleep ${REFRESH_SECONDS}
   local active_runs=$(gh run list --json status,headBranch --jq 'map(select(.headBranch == "'${BRANCH_NAME}'" and (.status == "in_progress" or .status == "queued"))) | length'
 )
 
@@ -24,7 +25,7 @@ wait_for_workflow_completion() {
 
 for i in {1..6}
 do
-  wait_for_workflow_completion()
+  wait_for_workflow_completion
   echo ">>> $i. Empty commit: (p$i)"
   git commit --allow-empty -m "${COMMIT_BASE_NAME} p$i" && git push ${REMOTE_NAME} ${BRANCH_NAME}
 done
