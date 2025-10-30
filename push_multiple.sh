69ca288a61cf6c27a1063b3814882e1c37f4a114
dev <vecarlos@brightsector.com>
2025-10-30 10:26:19 -0300

    branches

--- push_multiple.sh
+++ push_multiple.sh
@@ -3,15 +3,14 @@
 DELAY_SECONDS=100 #Change accordingly to build time
 
 COMMIT_BASE_NAME="Fluctuation test commit"
-BRANCH_NAME="fluctuation_test_$(date +%Y_%m_%d_%H:%M:%S)"
+BRANCH_NAME="releases/fluctuation_test_$(date +%Y_%m_%d_%H_%M_%S)"
 REMOTE_NAME="origin"
 git checkout -b ${BRANCH_NAME}
 
 for i in {1..6}
 do
   echo ">>> $i. Empty commit: (p$i)"
-  git commit --allow-empty -m "${COMMIT_BASE_NAME} p$i" && git push ${REMOTE_NAME} 
-${BRANCH_NAME}
+  git commit --allow-empty -m "${COMMIT_BASE_NAME} p$i" && git push ${REMOTE_NAME} ${BRANCH_NAME}
   echo -e "\n--- Waiting ${DELAY_SECONDS} seconds"
   sleep ${DELAY_SECONDS}
 done
