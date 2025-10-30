01183deac109b1fa96ec0cc94459ff5bc049d5f1
dev <vecarlos@brightsector.com>
2025-10-30 10:59:49 -0300

    sleep

--- push_multiple.sh
+++ push_multiple.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 
-DELAY_SECONDS=100 #Change accordingly to build time
 
+SLEEP_SECONDS=30 
 COMMIT_BASE_NAME="Fluctuation test commit"
 BRANCH_NAME="releases/fluctuation_test_$(date +%Y_%m_%d_%H_%M_%S)"
 REMOTE_NAME="origin"
+
 git checkout -b ${BRANCH_NAME}
+
+
+wait_for_workflow_completion() {
+  local active_runs=$(gh run list --json status,headBranch --jq 'map(select(.headBranch == "'${BRANCH_NAME}'" and (.status == "in_progress" or .status == "queued"))) | length'
+)
+
+  echo "Checking if active workflows"
+  while [[ "$active_runs" -gt 0 ]]; do
+    echo -e "\n--- Waiting ${SLEEP_SECONDS} seconds"
+    sleep ${SLEEP_SECONDS}
+    active_runs=$(gh run list --json status,headBranch --jq 'map(select(.headBranch == "'${BRANCH_NAME}'" and (.status == "in_progress" or .status == "queued"))) | length'
+)
+  done
+}
 
 for i in {1..6}
 do
+  wait_for_workflow_completion()
   echo ">>> $i. Empty commit: (p$i)"
   git commit --allow-empty -m "${COMMIT_BASE_NAME} p$i" && git push ${REMOTE_NAME} ${BRANCH_NAME}
-  echo -e "\n--- Waiting ${DELAY_SECONDS} seconds"
-  sleep ${DELAY_SECONDS}
 done
 echo -e "\nAll process completed"
