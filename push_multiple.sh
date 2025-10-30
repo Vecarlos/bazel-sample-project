79dc76ac51ce5f86eaf5dec8900b2075182b5524
dev <vecarlos@brightsector.com>
2025-10-30 10:15:41 -0300

    push multiple

--- push_multiple.sh
+++ push_multiple.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-COMMIT_BASE_NAME="all python test."
-BRANCH_NAME="buildbuddy"
+DELAY_SECONDS=100 #Change accordingly to build time
+
+COMMIT_BASE_NAME="Fluctuation test commit"
+BRANCH_NAME="fluctuation_test_$(date +%Y_%m_%d_%H:%M:%S)"
 REMOTE_NAME="origin"
-DELAY_SECONDS=1200
+git checkout -b ${BRANCH_NAME}
 
-echo ">>> 1. Add and commit"
-git add .
-git commit -m "$COMMIT_BASE_NAME" && git push $REMOTE_NAME $BRANCH_NAME
-
-for i in {2..15}
+for i in {1..6}
 do
-  echo -e "\n--- Waiting $DELAY_SECONDS seconds"
-  sleep $DELAY_SECONDS
-
   echo ">>> $i. Empty commit: (p$i)"
-  git commit --allow-empty -m "$COMMIT_BASE_NAME p$i" && git push $REMOTE_NAME $BRANCH_NAME
+  git commit --allow-empty -m "${COMMIT_BASE_NAME} p$i" && git push ${REMOTE_NAME} 
+${BRANCH_NAME}
+  echo -e "\n--- Waiting ${DELAY_SECONDS} seconds"
+  sleep ${DELAY_SECONDS}
 done
 echo -e "\nAll process completed"
