c6d84f199728fa0a6124a0ea49f42f89e70309a8
dev <vecarlos@brightsector.com>
2025-10-31 14:58:55 -0300

    Fluctuation test commit p1

--- push_multiple.sh
+++ push_multiple.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-SLEEP_SECONDS=30 
+SLEEP_SECONDS=5 
 COMMIT_BASE_NAME="Fluctuation test commit"
 BRANCH_NAME="buildbuddy_kt_cov"
 REMOTE_NAME="origin"
@@ -16,15 +16,15 @@ wait_for_workflow_completion() {
   local active_runs=$(gh run list --json status,headBranch --jq 'map(select(.headBranch == "'${branch_to_check}'" and (.status == "in_progress" or .status == "queued"))) | length')
 
   while [[ "$active_runs" -gt 0 ]]; do
-    echo "--- Workflow activo en '$branch_to_check'. Esperando ${POLL_SECONDS}s..."
-    sleep ${POLL_SECONDS}
+    echo "--- Workflow activo en '$branch_to_check'. Esperando ${SLEEP_SECONDS}s..."
+    sleep ${SLEEP_SECONDS}
     active_runs=$(gh run list --json status,headBranch --jq 'map(select(.headBranch == "'${branch_to_check}'" and (.status == "in_progress" or .status == "queued"))) | length')
   done
 }
 
 for i in {1..6}
 do
-  wait_for_workflow_completion
+  wait_for_workflow_completion $BRANCH_NAME
   echo ">>> $i. Empty commit: (p$i)"
   git commit --allow-empty -m "${COMMIT_BASE_NAME} p$i" && git push ${REMOTE_NAME} ${BRANCH_NAME}
 done
