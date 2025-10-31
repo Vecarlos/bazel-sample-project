13acf3c9bd1d8d6550481b34041b804fcf6134c7
dev <vecarlos@brightsector.com>
2025-10-31 14:30:02 -0300

    BRANCH A 5: Removed injected functions

--- src/main/kotlin/org/wfanet/measurement/common/grpc/RateLimiterProvider.kt
+++ src/main/kotlin/org/wfanet/measurement/common/grpc/RateLimiterProvider.kt
@@ -95,14 +95,3 @@ class RateLimiterProvider(
 
 
 
-// --- INJECTED FOR CACHE TEST ---
-fun injectedFunction1() {
-    println("Injected function 1 executed")
-}
-fun injectedFunction2() {
-    println("Injected function 2 executed")
-}
-fun injectedFunction3() {
-    println("Injected function 3 executed")
-}
-// --- END INJECTED ---
