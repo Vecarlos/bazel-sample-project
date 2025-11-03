22193f5ba8895aa6c07f36d8affa906dea255b28
dev <vecarlos@brightsector.com>
2025-11-03 13:13:16 -0300

    VICTIM 6: Created test targets

--- src/main/kotlin/org/wfanet/measurement/common/grpc/Interceptors.kt
+++ src/main/kotlin/org/wfanet/measurement/common/grpc/Interceptors.kt
@@ -112,3 +112,15 @@ fun BindableService.withInterceptors(
 ): ServerServiceDefinition = ServerInterceptors.intercept(this, *interceptors)
 
 
+
+// --- INJECTED FOR CACHE TEST ---
+fun injectedFunction1() {
+    println("Injected function 1 executed")
+}
+fun injectedFunction2() {
+    println("Injected function 2 executed")
+}
+fun injectedFunction3() {
+    println("Injected function 3 executed")
+}
+// --- END INJECTED ---
