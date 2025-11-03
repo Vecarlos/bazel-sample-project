a2b1a3834a074939deb8cba45acc2727c851dc28
dev <vecarlos@brightsector.com>
2025-11-03 10:25:47 -0300

    Comentar RateLimiterProviderTest

--- copy_push_multiple.sh
+++ copy_push_multiple.sh
@@ -46,7 +46,9 @@ do
 
   if [ $(($i % 2)) -eq 1 ]; then
     echo "Comentando RateLimiterProviderTest..."
-    sed -i '/kt_jvm_test(/{:a;N;/\n[ \t]*),?\r?$/!ba;/name = "RateLimiterProviderTest"/s/\n/\n#/g;s/^/#/}' "$BUILD_FILE" || true
+    sed -i '/kt_jvm_test(/,/^)/ {
+      /name = "RateLimiterProviderTest"/,/^)/ s/^/#/
+    }' "$BUILD_FILE"
     commit_msg="Comentar RateLimiterProviderTest"
   else
     echo "Descomentando RateLimiterProviderTest..."
