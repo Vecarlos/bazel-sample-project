9d8649da2200887b85f5db5742b8bb70e7125808
dev <vecarlos@brightsector.com>
2025-11-05 16:08:56 -0300

    VICTIM 1: Deleted test targets

--- src/main/kotlin/org/wfanet/measurement/common/grpc/Interceptors.kt
+++ src/main/kotlin/org/wfanet/measurement/common/grpc/Interceptors.kt
@@ -118,14 +118,3 @@ fun BindableService.withInterceptors(
 
 
 
-// --- INJECTED FOR CACHE TEST ---
-fun injectedFunction1() {
-    println("Injected function 1 executed")
-}
-fun injectedFunction2() {
-    println("Injected function 2 executed")
-}
-fun injectedFunction3() {
-    println("Injected function 3 executed")
-}
-// --- END INJECTED ---
