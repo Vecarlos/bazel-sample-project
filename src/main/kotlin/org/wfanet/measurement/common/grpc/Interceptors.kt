8ef2bcef309c86e9d8568187a60fd1a900030da9
dev <vecarlos@brightsector.com>
2025-11-11 14:26:11 -0300

    ACTIVE 9: Created test targets, discomment test and add empty functions

--- src/main/kotlin/org/wfanet/measurement/common/grpc/Interceptors.kt
+++ src/main/kotlin/org/wfanet/measurement/common/grpc/Interceptors.kt
@@ -111,3 +111,14 @@ fun BindableService.withInterceptors(
   vararg interceptors: ServerInterceptor
 ): ServerServiceDefinition = ServerInterceptors.intercept(this, *interceptors)
 
+// --- INJECTED FOR CACHE TEST ---
+fun injectedFunction1() {
+    println("Injected function 1 executed")
+}
+fun injectedFunction2() {
+    println("Injected function 2 executed")
+}
+fun injectedFunction3() {
+    println("Injected function 3 executed")
+}
+// --- END INJECTED ---
