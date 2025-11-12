a1ebf6db71c688f5f51a0f644d1e4ae82d487c5d
dev <vecarlos@brightsector.com>
2025-11-12 11:39:30 -0300

    ACTIVE 1: Uncomment test and add empty functions

--- src/main/kotlin/org/wfanet/measurement/edpaggregator/service/internal/Errors.kt
+++ src/main/kotlin/org/wfanet/measurement/edpaggregator/service/internal/Errors.kt
@@ -296,3 +296,14 @@ class InvalidFieldValueException(
     mapOf(Errors.Metadata.FIELD_NAME to fieldName),
     cause,
   )
+// --- INJECTED FOR CACHE TEST ---
+fun injectedFunction1() {
+    println("Injected function 1 executed")
+}
+fun injectedFunction2() {
+    println("Injected function 2 executed")
+}
+fun injectedFunction3() {
+    println("Injected function 3 executed")
+}
+// --- END INJECTED ---
