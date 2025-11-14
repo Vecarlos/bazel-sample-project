1a3594d2ab11f45924fea147a46737e9a5347134
dev <vecarlos@brightsector.com>
2025-11-14 17:39:38 -0300

    ACTIVE 3: Uncomment test and delete empty functions

--- src/main/kotlin/org/wfanet/measurement/eventdataprovider/requisition/v2alpha/common/FrequencyVectorBuilder.kt
+++ src/main/kotlin/org/wfanet/measurement/eventdataprovider/requisition/v2alpha/common/FrequencyVectorBuilder.kt
@@ -322,3 +322,14 @@ class FrequencyVectorBuilder(
       FrequencyVectorBuilder(populationSpec, measurementSpec, frequencyVector).apply(bind).build()
   }
 }
+// --- INJECTED FOR CACHE TEST ---
+fun injectedFunction1() {
+    println("Injected function 1 executed")
+}
+fun injectedFunction2() {
+    println("Injected function 2 executed")
+}
+fun injectedFunction3() {
+    println("Injected function 3 executed")
+}
+// --- END INJECTED ---
