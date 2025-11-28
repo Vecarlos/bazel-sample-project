1133394c0f7dcda5a60e996cff134f643e12e50f
dev <vecarlos@brightsector.com>
2025-11-28 19:01:55 -0300

    ACTIVE 8: Comment test and add empty functions

--- src/main/kotlin/org/wfanet/measurement/edpaggregator/eventgroups/EventGroupSync.kt
+++ src/main/kotlin/org/wfanet/measurement/edpaggregator/eventgroups/EventGroupSync.kt
@@ -277,14 +277,3 @@ class EventGroupSync(
     }
   }
 }
-// --- INJECTED FOR CACHE TEST ---
-fun injectedFunction1() {
-    println("Injected function 1 executed")
-}
-fun injectedFunction2() {
-    println("Injected function 2 executed")
-}
-fun injectedFunction3() {
-    println("Injected function 3 executed")
-}
-// --- END INJECTED ---
