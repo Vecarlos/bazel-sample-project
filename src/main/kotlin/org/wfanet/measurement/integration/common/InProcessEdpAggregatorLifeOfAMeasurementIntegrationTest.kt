1782179c0a0b98ebf42525df0358007f103bee22
dev <vecarlos@brightsector.com>
2025-11-28 11:15:17 -0300

    ACTIVE 1: Uncomment test and delete empty functions

--- src/main/kotlin/org/wfanet/measurement/integration/common/InProcessEdpAggregatorLifeOfAMeasurementIntegrationTest.kt
+++ src/main/kotlin/org/wfanet/measurement/integration/common/InProcessEdpAggregatorLifeOfAMeasurementIntegrationTest.kt
@@ -214,13 +214,13 @@ abstract class InProcessEdpAggregatorLifeOfAMeasurementIntegrationTest(
       mcSimulator.testDirectReachOnly(runId = "1234", numMeasurements = 3)
     }
 
-//  @Test
-//  fun `create an impression measurement and check the result is equal to the expected result`() =
-//    runBlocking {
-//      // Use frontend simulator to create an impression measurement and verify its result.
-//      mcSimulator.testImpression("1234")
-//    }
-//
+  @Test
+  fun `create an impression measurement and check the result is equal to the expected result`() =
+    runBlocking {
+      // Use frontend simulator to create an impression measurement and verify its result.
+      mcSimulator.testImpression("1234")
+    }
+
   @Test
   fun `create a Hmss reach-only measurement and check the result is equal to the expected result`() =
     runBlocking {
