9df5837a6d15a0fc8fe3a29240817884661f164b
dev <vecarlos@brightsector.com>
2025-12-01 10:08:46 -0300

    ACTIVE 1: Uncomment test and delete empty functions

--- src/main/kotlin/org/wfanet/measurement/reporting/service/api/CelEnvProvider.kt
+++ src/main/kotlin/org/wfanet/measurement/reporting/service/api/CelEnvProvider.kt
@@ -250,4 +250,22 @@ fun injectedFunction2() {
 fun injectedFunction3() {
     println("Injected function 3 executed")
 }
+fun injectedFunction4() {
+    println("Injected function 4 executed")
+}
+fun injectedFunction5() {
+    println("Injected function 5 executed")
+}
+fun injectedFunction6() {
+    println("Injected function 6 executed")
+}
+fun injectedFunction7() {
+    println("Injected function 7 executed")
+}
+fun injectedFunction8() {
+    println("Injected function 8 executed")
+}
+fun injectedFunction9() {
+    println("Injected function 9 executed")
+}
 // --- END INJECTED ---
