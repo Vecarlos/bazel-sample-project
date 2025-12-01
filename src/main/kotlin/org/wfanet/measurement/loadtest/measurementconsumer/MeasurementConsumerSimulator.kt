8352011fa632af533643609bc064e7803fb8de32
dev <vecarlos@brightsector.com>
2025-12-01 16:24:47 -0300

    ACTIVE 1: Uncomment test and delete empty functions

--- src/main/kotlin/org/wfanet/measurement/loadtest/measurementconsumer/MeasurementConsumerSimulator.kt
+++ src/main/kotlin/org/wfanet/measurement/loadtest/measurementconsumer/MeasurementConsumerSimulator.kt
@@ -501,13 +501,14 @@ abstract class MeasurementConsumerSimulator(
 
     // Get the CMMS computed result and compare it with the expected result.
 //    var reachOnlyResult = getReachResult(measurementName)
-    var reachOnlyResult = ""
+    var reachOnlyResult: Measurement.Result? = null
+
     var attemptCount = 0
     while (reachOnlyResult == null && (attemptCount < 6)) {
       attemptCount++
       logger.info { "################# STILL INSIDE THE POOL FOR RESULT R" }
       logger.info("Computation not done yet, wait for another 30 seconds.  Attempt $attemptCount")
-      delay(Duration.ofSeconds(60))
+      delay(Duration.ofSeconds(30))
       reachOnlyResult = getReachResult(measurementName)
     }
     logger.info { "################# OUT OF THE POOL FOR RESULT R" }
