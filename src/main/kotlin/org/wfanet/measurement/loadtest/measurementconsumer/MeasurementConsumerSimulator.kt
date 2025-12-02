a10048f18dc1bbe49eda5170d1026546ce7906ba
dev <vecarlos@brightsector.com>
2025-12-02 11:44:11 -0300

    ACTIVE 1: Uncomment test and delete empty functions

--- src/main/kotlin/org/wfanet/measurement/loadtest/measurementconsumer/MeasurementConsumerSimulator.kt
+++ src/main/kotlin/org/wfanet/measurement/loadtest/measurementconsumer/MeasurementConsumerSimulator.kt
@@ -208,17 +208,17 @@ abstract class MeasurementConsumerSimulator(
     logger.info { "Creating reach and frequency Measurement..." }
     // Create a new measurement on behalf of the measurement consumer.
     val measurementConsumer = getMeasurementConsumer(measurementConsumerData.name)
-//    val measurementInfo: MeasurementInfo =
-//      createMeasurement(
-//        measurementConsumer,
-//        runId,
-//        ::newReachAndFrequencyMeasurementSpec,
-//        requiredCapabilities,
-//        vidSamplingInterval = vidSamplingInterval,
-//        eventGroupFilter = eventGroupFilter,
-//      )
-//    val measurementName = measurementInfo.measurement.name
-//    logger.info { "Created reach and frequency Measurement $measurementName" }
+    val measurementInfo: MeasurementInfo =
+      createMeasurement(
+        measurementConsumer,
+        runId,
+        ::newReachAndFrequencyMeasurementSpec,
+        requiredCapabilities,
+        vidSamplingInterval = vidSamplingInterval,
+        eventGroupFilter = eventGroupFilter,
+      )
+    val measurementName = measurementInfo.measurement.name
+    logger.info { "Created reach and frequency Measurement $measurementName" }
 //
 //    onMeasurementsCreated?.invoke()
 
