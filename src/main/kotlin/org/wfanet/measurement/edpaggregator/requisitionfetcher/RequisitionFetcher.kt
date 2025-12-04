a0d346efd051322ac88f1a88cc5d8812b0169e23
dev <vecarlos@brightsector.com>
2025-12-04 10:46:07 -0300

    ACTIVE 5: Uncomment test and delete empty functions

--- src/main/kotlin/org/wfanet/measurement/edpaggregator/requisitionfetcher/RequisitionFetcher.kt
+++ src/main/kotlin/org/wfanet/measurement/edpaggregator/requisitionfetcher/RequisitionFetcher.kt
@@ -130,7 +130,7 @@ class RequisitionFetcher(
    */
   private suspend fun storeRequisitions(requisitions: List<Requisition>): Int =
     withStoreTelemetry(requisitions.size) {
-      delay(10000)
+      delay(5000)
       val groupedRequisitions = requisitionGrouper.groupRequisitions(requisitions)
       delay(5000)
 
