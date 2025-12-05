c3aa4461c54ade20d0db7a20c72acafcbeab0020
dev <vecarlos@brightsector.com>
2025-12-05 16:23:53 -0300

    ACTIVE 1: Uncomment test and delete empty functions

--- src/main/kotlin/org/wfanet/measurement/duchy/herald/Herald.kt
+++ src/main/kotlin/org/wfanet/measurement/duchy/herald/Herald.kt
@@ -121,7 +121,7 @@ class Herald(
     // Use custom retry logic to handle the stream potentially being partially processed.
     var attemptNumber = 1
     while (coroutineContext.isActive) {
-      delay(Duration.ofSeconds(15))
+//      delay(Duration.ofSeconds(15))
       try {
         syncStatuses()
       } catch (e: StreamingException) {
@@ -219,7 +219,7 @@ class Herald(
     logger.fine("[id=$globalId]: Processing updated GlobalComputation")
     val state = computation.state
     @Suppress("WHEN_ENUM_CAN_BE_NULL_IN_JAVA") // Proto enum fields are never null.
-    delay(Duration.ofSeconds(15))
+//    delay(Duration.ofSeconds(15))
     when (state) {
       // Creates a new computation if it is not already present in the database.
       State.PENDING_REQUISITION_PARAMS -> createComputation(computation)
