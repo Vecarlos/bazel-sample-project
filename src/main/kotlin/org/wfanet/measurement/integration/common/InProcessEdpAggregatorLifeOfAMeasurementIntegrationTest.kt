7fcd34be47b32f5716fbf05fcb05a502877a3183
vecarlos <vecarlos@brightsector.com>
2026-02-11 17:18:53 -0300

    ACTIVE 1: Uncomment test and delete empty functions

--- src/main/kotlin/org/wfanet/measurement/integration/common/InProcessEdpAggregatorLifeOfAMeasurementIntegrationTest.kt
+++ src/main/kotlin/org/wfanet/measurement/integration/common/InProcessEdpAggregatorLifeOfAMeasurementIntegrationTest.kt
@@ -186,21 +186,23 @@ abstract class InProcessEdpAggregatorLifeOfAMeasurementIntegrationTest(
     }
   }
 
-//  @Test
-//  fun `create a direct reach only measurement and check the result is equal to the expected result`() =
-//    runBlocking {
-//      // Use frontend simulator to create a direct reach and frequency measurement and verify its
-//      // result.
-//      mcSimulator.testDirectReachOnly(runId = "1234", numMeasurements = 1)
-//    }
-
   @Test
-  fun `create a direct RF measurement and check the result is equal to the expected result`() =
+  fun `create a direct reach only measurement and check the result is equal to the expected result`() =
     runBlocking {
       // Use frontend simulator to create a direct reach and frequency measurement and verify its
       // result.
+      mcSimulator.testDirectReachOnly(runId = "1234", numMeasurements = 1)
       mcSimulator.testDirectReachAndFrequency(runId = "1234", numMeasurements = 1)
+
     }
+
+//  @Test
+//  fun `create a direct RF measurement and check the result is equal to the expected result`() =
+//    runBlocking {
+//      // Use frontend simulator to create a direct reach and frequency measurement and verify its
+//      // result.
+//      mcSimulator.testDirectReachAndFrequency(runId = "1234", numMeasurements = 1)
+//    }
 
   // @Test
   // fun `create incremental direct reach only measurements in same report and check the result is equal to the expected result`() =
