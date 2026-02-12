04390e288be0522f72868bd00105d265d3330eee
vecarlos <vecarlos@brightsector.com>
2026-02-12 16:56:44 -0300

    ACTIVE 7: Uncomment test and delete empty functions

--- src/main/kotlin/org/wfanet/measurement/integration/common/InProcessEdpAggregatorLifeOfAMeasurementIntegrationTest.kt
+++ src/main/kotlin/org/wfanet/measurement/integration/common/InProcessEdpAggregatorLifeOfAMeasurementIntegrationTest.kt
@@ -105,7 +105,7 @@ abstract class InProcessEdpAggregatorLifeOfAMeasurementIntegrationTest(
       modelLineInfoMap = modelLineInfoMap,
     )
 
-//  @Before
+  @ClassRule
   fun setup() {
     runBlocking {
       pubSubClient.createTopic(PROJECT_ID, FULFILLER_TOPIC_ID)
@@ -310,7 +310,6 @@ abstract class InProcessEdpAggregatorLifeOfAMeasurementIntegrationTest(
     @JvmStatic
     fun initConfig() {
       InProcessCmmsComponents.initConfig()
-      setup()
     }
 
     @get:ClassRule @JvmStatic val pubSubEmulatorProvider = GooglePubSubEmulatorProvider()
