2ad0ad451c63bb5cdb417514afa33cb62dc33311
vecarlos <vecarlos@brightsector.com>
2026-02-13 09:50:46 -0300

    ACTIVE 1: Uncomment test and delete empty functions

--- src/main/kotlin/org/wfanet/measurement/integration/common/InProcessEdpAggregatorLifeOfAMeasurementIntegrationTest.kt
+++ src/main/kotlin/org/wfanet/measurement/integration/common/InProcessEdpAggregatorLifeOfAMeasurementIntegrationTest.kt
@@ -194,22 +194,22 @@ abstract class InProcessEdpAggregatorLifeOfAMeasurementIntegrationTest(
       mcSimulator.testDirectReachAndFrequency(runId = "1234", numMeasurements = 1)
     }
 
+//  @Test
+//  fun `create a direct reach only measurement and check the result is equal to the expected result`() =
+//    runBlocking {
+//      // Use frontend simulator to create a direct reach and frequency measurement and verify its
+//      // result.
+//      mcSimulator.testDirectReachOnly(runId = "1234", numMeasurements = 1)
+//    }
+
   @Test
-  fun `create a direct reach only measurement and check the result is equal to the expected result`() =
+  fun `create incremental direct reach only measurements in same report and check the result is equal to the expected result`() =
     runBlocking {
-      // Use frontend simulator to create a direct reach and frequency measurement and verify its
-      // result.
-      mcSimulator.testDirectReachOnly(runId = "1234", numMeasurements = 1)
+      // Use frontend simulator to create N incremental direct reach and frequency measurements and
+      // verify its result.
+      mcSimulator.testDirectReachOnly(runId = "1234", numMeasurements = 3)
     }
 
-//  @Test
-//  fun `create incremental direct reach only measurements in same report and check the result is equal to the expected result`() =
-//    runBlocking {
-//      // Use frontend simulator to create N incremental direct reach and frequency measurements and
-//      // verify its result.
-//      mcSimulator.testDirectReachOnly(runId = "1234", numMeasurements = 3)
-//    }
-//
 //  @Test
 //  fun `create an impression measurement and check the result is equal to the expected result`() =
 //    runBlocking {
